47f65c2bd35f829d27156e489e185e267680f94a
JadingTsunami <JadingTsunami@users.noreply.github.com>
2023-06-17 17:09:33 -0700

    Initial commit.

---
+++ analysis_tool/big_analysis.py
@@ -0,0 +1,38 @@
+from protobuf_decoder.protobuf_decoder import Parser
+import sys
+import struct
+
+def read7bit(f):
+    more = True
+    val = 0
+    order = 0
+    while more:
+        num = int.from_bytes(f.read(1), 'big')
+        more = ((num >> 7) & 0x01)
+        val += ((num&0x7f) << (order*7))
+        order += 1
+
+    return val
+
+with open("bigdata/bigfile.decomp", "rb") as f:
+    total_chunks = struct.unpack('i', f.read(4))[0]
+    print("Chunks: " + str(total_chunks))
+    for i in range(total_chunks):
+        s1len = read7bit(f)
+        s1 = f.read(s1len)
+        print(str(s1, 'UTF-8'))
+
+        s2len = read7bit(f)
+        s2 = f.read(s2len)
+        print(str(s2, 'UTF-8'))
+
+        protobuf_size = struct.unpack('i', f.read(4))[0]
+        print("Size: " + str(protobuf_size) + " bytes")
+        sys.stdout.flush()
+        protobuf = f.read(protobuf_size)
+        #parsed_data = Parser().parse(protobuf.hex())
+        #for d in parsed_data:
+        #    print(d)
+        #print("HEX")
+        #print(protobuf.hex())
+    print("Done")
---
+++ compression_tool/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+public static class BigFileCompression
+{
+    private const string CompressedFileName = "bigfile";
+    private const string ReCompressedFileName = "bigfile.recomp";
+    private const string ModFileName = "bigfile.mod";
+    private const string DecompressedFileName = "bigfile.decomp";
+
+    public static void Main()
+    {
+        if (File.Exists(ModFileName)) {
+            CompressFile();
+            Console.WriteLine($"Created compressed file '{ReCompressedFileName}'.");
+        }
+        if (!File.Exists(DecompressedFileName)) {
+            DecompressFile();
+            Console.WriteLine($"Decompressed file '{DecompressedFileName}'.");
+        }
+    }
+
+    private static void CompressFile()
+    {
+        using FileStream modFileStream = File.Open(ModFileName, FileMode.Open);
+        using FileStream compressedFileStream = File.Create(ReCompressedFileName);
+        using var compressor = new DeflateStream(compressedFileStream, CompressionMode.Compress);
+        modFileStream.CopyTo(compressor);
+    }
+
+    private static void DecompressFile()
+    {
+        using FileStream compressedFileStream = File.Open(CompressedFileName, FileMode.Open);
+        using FileStream outputFileStream = File.Create(DecompressedFileName);
+        using var decompressor = new DeflateStream(compressedFileStream, CompressionMode.Decompress);
+        decompressor.CopyTo(outputFileStream);
+    }
+}
