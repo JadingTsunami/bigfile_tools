93ca9b740cff84496a57476de320e39f474c4f05
JadingTsunami <JadingTsunami@users.noreply.github.com>
2023-06-25 16:59:38 -0700

    Added more known types. Need to allow star matching with more depth.

--- bigfile_editor/known_types.py
+++ bigfile_editor/known_types.py
@@ -1,18 +1,41 @@
+"""
+Look up known types inside the bigfile to provide a user-friendly meaning for
+the field.
+
+
+known_types:
+    keys: tables in bigfile
+    values: dictionary of next-level lookup for each table
+
+table lookup dictionary:
+    keys: either a number or '*' (wildcard)
+        if the exact match is in the dictionary, it's used
+        else the '*' will match if present
+    values: nested dictionary OR
+            'name' used for the top-level name OR
+            string with a value
+
+The "name" key exists so you can give a name to a section while still giving names to entries beneath it in the hierarchy.
+"""
 known_types = {
     "MetaGameConfigData" : {
         "1" : {
             "name" : "Character definitions",
-            "0" : {
-                "name": "Axel4 Character"
-            }
+            "0" : "Axel4 Character",
+            "1" : "Blaze4 Character",
+            "2" : "Cherry Character"
         },
         "14" : {
-            "1" : {
-                "name": "Hud Icon"
-            }
+            "1" : "Hud Icon"
         },
         "29": {
             "name": "Difficulty definitions",
+            "0": "Easy",
+            "1": "Normal",
+            "2": "Hard",
+            "3": "Hardest",
+            "4": "Mania",
+            "5": "Mania Plus",
             "*": {
                 "1": "Difficulty name",
                 "4": {
@@ -58,6 +81,10 @@ def lookup_type(crumb):
     kt = ''
     type_lookup = known_types
     for chunk in crumb:
+        if isinstance(type_lookup, str):
+            kt = ''
+            type_lookup = {}
+            break
         if chunk in type_lookup:
             type_lookup = type_lookup[chunk]
         elif "*" in type_lookup:
