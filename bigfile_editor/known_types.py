d4c174f28a1c17ba3b4f45bc298f330d58fee2ae
JadingTsunami <JadingTsunami@users.noreply.github.com>
2023-06-25 16:39:11 -0700

    Add some more known types.

--- bigfile_editor/known_types.py
+++ bigfile_editor/known_types.py
@@ -10,7 +10,31 @@ known_types = {
             "1" : {
                 "name": "Hud Icon"
             }
-        }
+        },
+        "29": {
+            "name": "Difficulty definitions",
+            "*": {
+                "1": "Difficulty name",
+                "4": {
+                    "13": "Starting lives",
+                    "19": "Extra life points (story/level select)",
+                    "20": "Extra life points (arcade)"
+                }
+            }
+        },
+        "100": {
+            "name": "Survival unlocks",
+            "*": {
+                "1": "Unlockable",
+                "2": "Required score",
+                "3": "Unlocked character"
+            }
+        },
+        "106": "PvP Arenas",
+        "114": "Training levels",
+        "115": "Free Training",
+        "116": "Survival config",
+        "117": "(Maybe) Training enemy spawner"
     },
 
     "CharacterData" : {
@@ -42,6 +66,8 @@ def lookup_type(crumb):
             kt = ''
             type_lookup = {}
             break
-    if "name" in type_lookup:
+    if isinstance(type_lookup, dict) and "name" in type_lookup:
         kt = type_lookup["name"]
+    elif isinstance(type_lookup, str):
+        kt = type_lookup
     return kt
